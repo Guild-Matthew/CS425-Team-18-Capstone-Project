f790bce213df2780fd80b5278dbe99ad78cbc9e1
Petree-Shane <77944815+Petree-Shane@users.noreply.github.com>
2025-02-09 12:28:29 -0800

    Delete CapstoneProject directory
    This is to fix the weird file system that's currently in place.

--- CapstoneProject/HelloFlask/static/js/voiduser.js
+++
@@ -1,8 +0,0 @@
-//Matthew Guild
-function filterAccounts() {
-    // Get the selected filter type
-    const filterType = document.getElementById('filterType').value;
-
-    window.location.href = `/VoidStudent?filterType=${filterType}`;
-}
-
