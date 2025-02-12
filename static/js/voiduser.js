a7391b3fc5b10a274e3d9af3d04dd72d1e7d8ba5
Guilherme Cassiano <89324951+GuilhermeCassiano1@users.noreply.github.com>
2025-02-12 15:39:19 -0800

    Delete static directory
    Wrong commit

--- static/js/voiduser.js
+++
@@ -1,7 +0,0 @@
-function filterAccounts() {
-    // Get the selected filter type
-    const filterType = document.getElementById('filterType').value;
-
-    window.location.href = `/VoidStudent?filterType=${filterType}`;
-}
-
