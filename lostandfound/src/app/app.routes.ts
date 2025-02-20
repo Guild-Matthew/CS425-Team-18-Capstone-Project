4aeca357746cd44a129382102a9624ce2edcfecd
Cottier-Mary <mcottier@nevada.unr.edu>
2025-02-19 18:30:45 -0800

    Login page modified
    Updated the login page the rest of the way to Angular complete with background.  Also adjusted the colorways and logo sizing on all updated pages so that they are consistent

--- lostandfound/src/app/app.routes.ts
+++ lostandfound/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AddUserComponent } from './add-user/add-user.component';
 import { ItemsComponent } from './items/items.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'adduser', component: AddUserComponent },
+  { path: 'add-user', component: AddUserComponent },
   { path: 'items', component: ItemsComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: 'dashboard' } // Handles unknown paths
 ];
