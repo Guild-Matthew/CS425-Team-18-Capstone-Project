c6edc42507c4a107f302dbe5822e53e2d751ada1
Petree-Shane <77944815+Petree-Shane@users.noreply.github.com>
2025-03-02 14:27:18 -0800

    Updating FlaskBackend to write the host and port info to a config.
    -Updating FlaskBackend runserver.py to write the host and port info to the flask-connection.config.json config.
    
    -Updated some nav bars on pages to not include a link to the current page.
    
    -Added some comments and suggestions in some files.

--- AngularFrontend/src/app/app.routes.ts
+++ AngularFrontend/src/app/app.routes.ts
@@ -6,24 +6,8 @@ import { LostAndFoundComponent } from './pages/lost-and-found/lost-and-found.com
 import { AddItemComponent } from './pages/add-item/add-item.component';
 import { ClaimedItemsComponent } from './pages/claimed-items/claimed-items.component';
 import { AddUserComponent } from './pages/add-user/add-user.component';
+//task map page doesn't work yet
 //import { MapComponent } from './map/map.component';
-
-// adding components for each page
-//import { LostFoundComponent } from './lost-found/lost-found.component';
-//import { AddItemComponent } from './add-item/add-item.component';
-//import { RemoveItemComponent } from './remove-item/remove-item.component';
-
-//import { ClaimedItemsComponent } from './claimed-items/claimed-items.component';
-//import { DeactivateuserComponent } from './deactivateuser/deactivateuser.component';
-//import { ItemsComponent } from './items/items.component';
-
-//import { LoginComponent } from './login/login.component';
-//import { UserHomeComponent } from './user-home/user-home.component';
-//import { AdminHomeComponent } from './admin-home/admin-home.component';
-
-//import { SuperHomeComponent } from './super-home/super-home.component';
-//import { AddUserComponent } from './adduser/adduser.component';
-//import { SuperadduserComponent } from './superadduser/superadduser.component';
 
 
 export const routes: Routes = [
@@ -39,10 +23,13 @@ export const routes: Routes = [
   //},
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+
+  //task there is not a way to get to add-user yet through the UI
   { path: 'add-user', component: AddUserComponent },
   { path: 'login', component: LoginComponent },
   { path: 'lost-and-found', component: LostAndFoundComponent },
   { path: 'add-item', component: AddItemComponent },
   { path: 'claimed-items', component: ClaimedItemsComponent },
+  //task this would be better if it was a error:404 page
   { path: '**', redirectTo: 'dashboard' } // Handles unknown paths
 ]
--- FlaskBackend/HelloFlask/runserver.py
+++ FlaskBackend/HelloFlask/runserver.py
@@ -1,12 +1,37 @@
 # Shane Petree
 import os
+import json
 from HelloFlask import app
 
+# writes the host IP and port number to a config file, for Angular to access
+# DO NOT READ FROM THIS CONFIG FILE UNTIL FLASK IS RUNNING, the IP and Port are inaccurate until flask runs
+# this config file could also be moved into AngularFrontend if it poses issues
+def write_config(host, port):
+    config = {'host': host, 'port': port}
+    with open('./HelloFlask/flask-connection.config.json', 'w') as config_file:
+            json.dump(config, config_file)
+
 if __name__ == '__main__':
+    
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
 
+    # write the host address and port number to the config file
+    write_config(HOST, PORT)
+
     app.run(HOST, PORT)
+
+    # another way to get the host IP and Port number?
+    """
+    HOST = os.getenv('SERVER_HOST', '127.0.0.1')
+    try:
+        PORT = int(os.getenv('SERVER_PORT', 5555))
+    except ValueError:
+        PORT = 5555
+
+    app.run(host=HOST, port=PORT)
+    """
+    # app.run(host ='localhost', port= 5000)
