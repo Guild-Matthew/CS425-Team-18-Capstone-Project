a3167278898cdcea6c0438bd1c899e911d6d52ad
Cottier-Mary <mcottier@nevada.unr.edu>
2025-03-28 15:12:27 -0700

    updated queries.py
    added reactivate user query and renamed delete user to deactivate

--- FlaskBackend/HelloFlask/queries.py
+++ FlaskBackend/HelloFlask/queries.py
@@ -165,10 +165,20 @@ class Queries:
         return [{"username": row[0], "email": row[1], "active": row[2], "role": row[3]} for row in rows] 
  
     # Query to deactivate an account 
-    def deleteUser(self, email, username):
+    def deactivateUser(self, email, username):
         query = """
         UPDATE users
         SET active = FALSE
+        WHERE email = %s AND username = %s
+        """
+        self.cursor.execute(query, (email, username))  
+        self.conn.commit()
+
+    # Query to reactivate an account that already exists 
+    def activateUser(self, email, username):
+        query = """
+        UPDATE users
+        SET active = TRUE
         WHERE email = %s AND username = %s
         """
         self.cursor.execute(query, (email, username))  
