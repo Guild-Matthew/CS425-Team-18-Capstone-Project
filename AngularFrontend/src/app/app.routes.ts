b5fb7ca62aa74247925ac4d1660ad174291fd874
Guilherme Cassiano <guilhermedominguescassiano@gmail.com>
2025-04-10 11:11:15 -0700

    Generate Report page created

--- AngularFrontend/src/app/app.routes.ts
+++ AngularFrontend/src/app/app.routes.ts
@@ -16,6 +16,7 @@ import { AddBuildingComponent } from './pages/add-building/add-building.componen
 import { DeactivateUserComponent } from './pages/deactivateuser/deactivateuser.component';
 import { LogoutComponent } from './pages/logout/logout.component';
 import { AddFloorComponent } from './pages/add-floor/add-floor.component';
+import { GenerateReportComponent } from './pages/generate-report/generate-report.component';
 
 export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
@@ -31,6 +32,7 @@ export const routes: Routes = [
   { path: 'add-building', component: AddBuildingComponent, canActivate: [AuthGuard] },
   { path: 'add-floor', component: AddFloorComponent, canActivate: [AuthGuard]},
   { path: 'deactivateuser', component: DeactivateUserComponent, canActivate: [AuthGuard] },
+  { path: 'generate-report', component: GenerateReportComponent, canActivate: [AuthGuard] },
   { path: '404', component: _404Component },
   { path: '**', redirectTo: '404' } // Handles unknown paths
 ];
